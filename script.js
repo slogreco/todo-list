50f510b5b10a80d306bd47d6d07a652bdc3381d3
Slogreco <shoshwright@hotmail.com>
2020-08-29 11:16:46 -0700

    initial code import

---
+++ script.js
@@ -0,0 +1,7 @@
+const todoInput = document.querySelector("#todo-text");
+const todoForm = document.querySelector("#todo-form");
+const todoList = document.querySelector("#todo-list");
+const todoCountSpan = document.querySelector("#todo-count");
+
+const todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+
