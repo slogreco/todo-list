b49c9024e37198eefca34e374d622c49747fe421
Slogreco <shoshwright@hotmail.com>
2020-08-29 11:26:37 -0700

    render todos

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ const todoCountSpan = document.querySelector("#todo-count");
 
 const todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
 
+renderTodos();
+
+function renderTodos() {
+    todoList.textContent = ("");
+    todoCountSpan.textContent = todos.length;
+}
+
+for (i = 0; i < todos.length; i++) {
+    let todo = todos[i];
+
+    let li = document.createElement("li");
+    li.textContent = todo;
+    todoList.appendChild(li);
+}
